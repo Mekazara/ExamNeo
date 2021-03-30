d0cbf7b5ba75ff7af42c27f07b35f20af6517317
Mekazara <mekuchazara@gmail.com>
2021-03-30 19:41:37 +0600

    new model added to admin panel

--- exam_project/courses/admin.py
+++ exam_project/courses/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from courses.models import Course, ProgrammingLanguage, CourseDetail
+from courses.models import Course, ProgrammingLanguage, \
+    CourseDetail, ChooseCourse
 
 admin.site.register(Course)
 admin.site.register(ProgrammingLanguage)
 admin.site.register(CourseDetail)
+admin.site.register(ChooseCourse)

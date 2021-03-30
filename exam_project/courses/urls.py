0cf45dab1cd8d6c81db25becf8acfc2892bf3a07
Mekazara <mekuchazara@gmail.com>
2021-03-30 19:43:07 +0600

    all new urls seted

--- exam_project/courses/urls.py
+++ exam_project/courses/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from courses.views import CoursePrLangViewset, NewCourseViewset
+from courses.views import CoursePrLangViewset, NewCourseViewset, \
+    ChooseCourseViewset, ReviewCourseViewset, CreatePrLangViewset, CreateCourseViewset
 
 router = routers.DefaultRouter()
 router.register('courseprlan', CoursePrLangViewset, basename='courseprlan')
 router.register('newcourse', NewCourseViewset, basename='newcourse')
+router.register('choosecourse', ChooseCourseViewset, basename='choosecourse')
+router.register('reviewcourses', ReviewCourseViewset, basename='reviewcourses')
+router.register('createcourse', CreateCourseViewset, basename='createcourse')
+router.register('createproglang', CreatePrLangViewset, basename='createproglang')
+
 
 urlpatterns = [
     path('', include(router.urls))

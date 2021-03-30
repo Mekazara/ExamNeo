afc72f46234cd9cbf7c69e4be46cecfa8a7fb9ea
Mekazara <mekuchazara@gmail.com>
2021-03-30 19:42:05 +0600

    ChooseCourse model created

--- exam_project/courses/models.py
+++ exam_project/courses/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Course(models.Model):
@@ -20,8 +21,19 @@ class CourseDetail(models.Model):
     duration = models.IntegerField()
     price_per_month = models.IntegerField()
 
-
-
     def __str__(self):
         return str(self.programming_language) + ' ' + str(self.title)
+
+class ChooseCourse(models.Model):
+    title = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, related_name='courses_chc')
+    programming_language = models.ForeignKey(ProgrammingLanguage, on_delete=models.SET_NULL,
+                                             null=True, related_name='pr_langs_chc')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(max_length=10)
+    full_name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return str(self.course_detail.title.title) + ' ' \
+               + str(self.course_detail.programming_language.programming_language)
 
